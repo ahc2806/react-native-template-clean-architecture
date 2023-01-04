f7f879fed5af2d03db5b0755ec27f673da69104a
Alonso Hernandez <54516859+ahc2806@users.noreply.github.com>
2023-01-04 13:27:41 -0600

    Added License and Husky JS for test commits

--- post-init.script.sh
+++ post-init.script.sh
@@ -2,3 +2,9 @@
 
 echo "\nInitializing git repository\n"
 git init
+
+echo "\nInitializing husky (Please hit return/enter key for the next prompt)\n"
+npx husky-init
+
+echo "\nInitializing husky hooks\n"
+npx husky set .husky/pre-commit "npm run test && npm run lint"
--- template.config.js
+++ template.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   placeholderName: 'ProjectName',
   templateDir: './template',
-  // postInitScript: './post-init.script.sh',
+  postInitScript: './post-init.script.sh',
 };
